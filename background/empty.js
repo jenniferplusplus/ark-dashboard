995b63eb6fa7698c97aa0c6853133da48d44c570
Greg Gellene <greg.gellene@gmail.com>
2016-10-21 19:29:11 -0500

    fixed fn call

--- background/empty.js
+++ background/empty.js
@@ -15,7 +15,7 @@ module.exports = function stopOnEmpty() {
     .then(function parseStatus(status){
       if(status.status === 'running'){
         if((+status.numberOfPlayers) < 1){
-          console.log(new Date().toString(), 'No players connected, shutdown scheduled');
+          console.log('No players connected, shutdown scheduled');
           pendingStop = true;
           timers.setTimeout(stop, config.server.serviceEmptyTime * 1000)
         }
@@ -23,14 +23,15 @@ module.exports = function stopOnEmpty() {
     });
 
   function stop() {
-    service.status()
+    service.serviceStatus()
       .then(function stop(status){
         if((+status.numberOfPlayers) < 1){
           service.stop();
-          console.log(new Date().toString(), 'No players connected, shutting down now');
+          console.log(status);
+          console.log('No players connected, shutting down now');
         }
         else{
-          console.log(new Date().toString(), 'Players connected, shutdown cancelled');
+          console.log('Players connected, shutdown cancelled');
         }
         pendingStop = false;
       });
